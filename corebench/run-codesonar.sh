46aee66d04bc4457d6d470293982bd772ad01c91
bstee615 <benjaminjsteenhoek@gmail.com>
2020-10-23 14:02:43 -0500

    Improve codesonar run script

--- corebench/run-codesonar.sh
+++ corebench/run-codesonar.sh
@@ -11,16 +11,16 @@ function usage()
 	quit "Usage: $0 <subject> <revision> </path/to/corerepo>. The project should be installed by install.sh first."
 }
 
-if [ -z $1 ] || [ ! -d $2  ] || [ ! -d $3 ]
-then
-	usage
-fi
 
 subject="$1"
 rev="$2"
 repo="$3"
-
 root="$repo/$subject/$rev/$subject"
+if [ -z $subject ] || [ -z $rev ] || [ ! -d $repo ] || [ ! -d $root ]
+then
+	usage
+fi
+
 echo "Building $root with CodeSonar..."
 pushd $root
 
@@ -31,6 +31,8 @@ then
 fi
 
 # Build with CodeSonar and report errors.
+printf "all: ;\nclean: ;" > po/Makefile
+printf "all: ;\nclean: ;" > doc/Makefile
 make clean \
 	&& codesonar analyze $PWD -project "/benjis/corebench/$subject.$rev" make \
 	|| quit "Error building $root."
