356853875797f04094690e11a1f48802aaf4deb5
bstee615 <benjaminjsteenhoek@gmail.com>
2020-10-26 22:24:28 -0500

    Add scratchpad of useful commands

---
+++ useful-commands.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# not_installed(subject). Expects to be run in ~/work/corebench/corerepo!
+function not_installed() {
+    for d in $(ls | grep -v master)
+    do
+        if [ -d $d ] && [ -z $(ls $d/$1 | grep is_installed) ]
+        then
+            echo $d
+        fi
+    done
+}
+
+# run_polyspace_for_file(subject). Expects input file too!
+function run_polyspace_for_file() {
+    for d in $(cat $1-easy-regressions.txt)
+    do
+        ./run-polyspace.sh $1 $d ~/work/corebench/corerepo/ ~/work/corebench/corebench/ ~/work/corebench/polyspace-reports
+    done
+}
